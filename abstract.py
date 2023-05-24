cf93f7d4f00bb6fe6d130ecf5147440bf2b9af89
Michal Moskal <michal.moskal@comarch.com>
2023-05-24 14:40:26 +0200

    update baza to instancja

--- abstract.py
+++ abstract.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-
+import typing as tp
 
 class BaseCommands(ABC):
     @abstractmethod
@@ -10,54 +10,56 @@ class BaseCommands(ABC):
 
     @abstractmethod
     def create_table(
-            self
+            self,
+            table_name: str = "imdbdata"
     ) -> None:
         pass
 
     @abstractmethod
     def drop_table(
             self,
-            table_name: str
+            table_name: str = "imdbdata"
     ) -> None:
         pass
 
     @abstractmethod
     def test_time_for_insert(
             self,
-            record_amount: int,
-            table_name: str
+            record_amount: int = 10000,
+            table_name: str = "imdbdata",
+            values: tp.Optional[tp.List] = None
     ) -> float:
         pass
 
     @abstractmethod
     def test_time_for_modify(
             self,
-            record_amount: int,
-            table_name: str
+            record_amount: int = 10000,
+            table_name: str = "imdbdata"
     ) -> float:
         pass
 
     @abstractmethod
     def test_time_for_delete(
             self,
-            record_amount: int,
-            table_name: str
+            record_amount: int = 10000,
+            table_name: str = "imdbdata"
     ) -> float:
         pass
 
     @abstractmethod
     def get_record_amount(
             self,
-            table_name: str
+            table_name: str = "imdbdata",
     ) -> int:
         pass
 
     @abstractmethod
     def test_time_for_record_amount_with_word(
             self,
-            table_name: str,
             field_name: str,
-            search_word: str
+            search_word: str,
+            table_name: str = "imdbdata"
     ) -> float:
         pass
 
